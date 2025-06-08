cc62d7efd17eb4e9970256e28d321b5dbc0f496d
sarlazalika <sarlazalika@gmail.com>
2025-06-08 14:12:55 +0700

    git add public/index.php git commit -m "add: router sederhana di public/index.php"

--- public/index.php
+++ public/index.php
@@ -1,12 +1,14 @@
 <?php
-include '../admin/koneksi.php';
-$artikel = mysqli_query($conn, "SELECT * FROM artikel ORDER BY tanggal DESC");
-?>
+session_start();
+if (!isset($_SESSION['admin'])) {
+    header("Location: login.php");
+    exit;
+}
+$controller = $_GET['controller'] ?? 'artikel';
+$action = $_GET['action'] ?? 'index';
 
-<h2>Artikel Publik</h2>
-<?php while ($row = mysqli_fetch_assoc($artikel)): ?>
-    <h3><?= $row['judul'] ?></h3>
-    <p><small><?= $row['tanggal'] ?></small></p>
-    <p><?= substr($row['isi'], 0, 100) ?>...</p>
-    <hr>
-<?php endwhile; ?>
+require_once "app/controllers/" . ucfirst($controller) . "Controller.php";
+$controllerName = ucfirst($controller) . "Controller";
+$controllerObj = new $controllerName;
+$controllerObj->$action();
+?>
